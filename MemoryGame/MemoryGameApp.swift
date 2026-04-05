3f432cdef39bbf9e19906194c902dce77a31e047
cestmoilouu <114187146+cestmoilouu@users.noreply.github.com>
2026-04-04 21:23:27 -0400

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Ludlyne Alysha Janvier on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGameTests/MemoryGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameTests.swift
+//  MemoryGameTests
+//
+//  Created by Ludlyne Alysha Janvier on 4/4/26.
+//
+
+import Testing
+@testable import MemoryGame
+
+struct MemoryGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
